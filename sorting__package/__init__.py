1339841b1fbe244902f9198340c6f4f972da931b
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-11-09 18:35:13 +0300

    Implement sorting package

---
+++ sorting__package/__init__.py
@@ -0,0 +1,6 @@
+from .quick_sort import quick_sort
+from .heap_sort import heap_sort
+from .merge_sort import merge_sort
+from .bubble_sort import bubble_sort
+
+__all__ = ["quick_sort", "heap_sort", "merge_sort", "bubble_sort"]
